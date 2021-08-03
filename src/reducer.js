f25671be509a87a48c102234078016306189a058
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-08-03 20:58:42 -0300

    agregué funcionalidad para sumar total de items

--- src/reducer.js
+++ src/reducer.js
@@ -7,6 +7,10 @@ export const actionTypes = {
   REMOVE_ITEM: "REMOVE_ITEM",
 };
 
+export const getBasketTotal = (basket) => {
+  basket?.reduce((amount, item) => item.price + amount, 0);
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
